a9b65a3eead2f7894f9d42b24a6c02e642731f1c
sami999khan999 <sami999kahn999@gmail.com>
2022-09-18 20:55:45 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+// dom manipulation
+
+// 1. selecting elements
+// getElementById()
+// const title = document.getElementById("main-heading");
+// const secondTitle = document.getElementById("list");
+
+// // getElementByClassName()
+// const country = document.getElementsByClassName("country");
+
+// //getElementByTagNsme()
+// const input = document.getElementsByTagName("input");
+
+// // querySelector()
+// const title2 = document.querySelector("#main-heading"); // id(#)
+// const countries = document.querySelectorAll(".country"); // class(.)
+// const input2 = document.querySelector(".btn");
+const list = document.querySelectorAll("li"); // tag()
+
+// // querySelectorAll()
+// const listAll = document.querySelectorAll("li");
+// const countrieALL = document.querySelectorAll(".country");
+// console.log(listAll);
+// 2. styling elements
+// 3. creating elementds
+// 4. node traversole
+// 5. event handelers
